0a50a2bba85c3978af36e394b190cce247e470b2
dark phoenix <akshit2022@outlook.com>
2023-07-14 14:38:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-#MIT License
-
-#Copyright (c) 2021 subinps
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
 from pyrogram.raw import functions, types
 from pyrogram import Client, idle
 from pyromod import listen
 from config import Config
-STATUS=Config.STATUS
+STATUS = Config.STATUS
 
-USER=Config.USER
+USER = Config.USER
 bot = Client(
     "InstaSessibon",
     bot_token=Config.BOT_TOKEN,
@@ -33,7 +12,7 @@ bot = Client(
     api_hash=Config.API_HASH,
     workers=50,
     plugins=dict(root="plugins")
-    )
+)
 
 
 async def main():
@@ -42,10 +21,7 @@ async def main():
         Config.L.load_session_from_file(USER, filename=f"./{USER}")
         STATUS.add(1)
 
+
 if Config.INSTA_SESSIONFILE_ID:
-    bot.run(main())
-
-bot.start()
-
-idle()
-bot.stop()
+    bot.run(main)
+    idle()
